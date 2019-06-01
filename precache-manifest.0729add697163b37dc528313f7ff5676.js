769187a697af30e9dc4041b95c99117f8f9bbf0f
GuanYangCLU <guanyang@callutheran.edu>
2019-05-31 22:51:23 -0700

    Updates

---
+++ precache-manifest.0729add697163b37dc528313f7ff5676.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd99f8e3744a0cf1335282d72cb77f5f",
+    "url": "/myPortfolio/static/media/email_icon.fd99f8e3.png"
+  },
+  {
+    "revision": "0d28f0ac596e8ea8a1a7",
+    "url": "/myPortfolio/static/css/main.6c72718f.chunk.css"
+  },
+  {
+    "revision": "2261391d3370eb3cf5eb",
+    "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
+  },
+  {
+    "revision": "63e7eaa6ca3fc987effe",
+    "url": "/myPortfolio/static/js/2.1f550898.chunk.js"
+  },
+  {
+    "revision": "9e3eecc7460d013e26ae67aa68c443df",
+    "url": "/myPortfolio/static/media/project1.9e3eecc7.png"
+  },
+  {
+    "revision": "f08332f86970971142840cbc7f9749c5",
+    "url": "/myPortfolio/static/media/project2.f08332f8.png"
+  },
+  {
+    "revision": "f18c73c224156ddb5f2fc1db973f323f",
+    "url": "/myPortfolio/static/media/project3.f18c73c2.png"
+  },
+  {
+    "revision": "9e3eecc7460d013e26ae67aa68c443df",
+    "url": "/myPortfolio/static/media/project4.9e3eecc7.png"
+  },
+  {
+    "revision": "32f7aa1963141f17b4cabd27473a04f9",
+    "url": "/myPortfolio/static/media/icons.32f7aa19.png"
+  },
+  {
+    "revision": "0d28f0ac596e8ea8a1a7",
+    "url": "/myPortfolio/static/js/main.338e473c.chunk.js"
+  },
+  {
+    "revision": "c88cc5d1881a428b050ab6257458e9de",
+    "url": "/myPortfolio/static/media/instagram_icon.c88cc5d1.png"
+  },
+  {
+    "revision": "bf4eaa72242a607866d7c9b97b687e77",
+    "url": "/myPortfolio/static/media/google-brands.bf4eaa72.svg"
+  },
+  {
+    "revision": "f08332f86970971142840cbc7f9749c5",
+    "url": "/myPortfolio/static/media/project5.f08332f8.png"
+  },
+  {
+    "revision": "9e3eecc7460d013e26ae67aa68c443df",
+    "url": "/myPortfolio/static/media/project6.9e3eecc7.png"
+  },
+  {
+    "revision": "84ea72bb8ecfe5e2ac12cc1dbe4eab44",
+    "url": "/myPortfolio/static/media/profile.84ea72bb.jpg"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/myPortfolio/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/myPortfolio/static/media/roboto-condensed.light.cf4449c1.ttf"
+  },
+  {
+    "revision": "c11c54b7f092de7b231e7f3526707707",
+    "url": "/myPortfolio/index.html"
+  }
+];
